783de0ff5a4ce82f3fb824ca7abfd57b60bd7dcf
Joel <jnfeinstein@gmail.com>
2014-03-17 01:15:18 -0700

    Implement some exit framework to clean up after ethernet.

--- index.js
+++ index.js
@@ -5,8 +5,3 @@ var switch1 = new Switch("eth2", "virtual");
 var switch2 = new Switch("virtual", "eth1");
 
 Device.connect(switch1.devices[1], switch2.devices[0]);
-
-process.on("SIGINT", function() {
-  Ethernet.cleanup();
-  process.exit();
-});
