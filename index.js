6a746fe2725db399f46b88717028e996bea49c56
Joel <jnfeinstein@gmail.com>
2014-03-17 01:05:53 -0700

    Updating index.js to use the requires file.

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-require('./lib/helpers');
-var Switch = require('./lib/components/switch');
-var Ethernet = require('./lib/devices/ethernet');
-var Virtual = require('./lib/devices/virtual');
+require('./lib/requires');
 
 var s = new Switch(2);
 var switch1 = new Switch("eth2", "virtual");
 var switch2 = new Switch("virtual", "eth1");
 
-Virtual.connect(switch1.devices[1], switch2.devices[0]);
+Device.connect(switch1.devices[1], switch2.devices[0]);
 
 process.on("SIGINT", function() {
   Ethernet.cleanup();
