97acccf19c95de4dae77d31af9476543cf086cd1
Joel <jnfeinstein@gmail.com>
2014-03-16 19:07:44 -0700

    Reorganized libraries, and changed how devices are stored in the switch.

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 require('./lib/helpers');
 var Switch = require('./lib/switch');
-var Ethernet = require('./lib/ethernet');
+var Virtual = require('./lib/devices/ethernet');
+var Virtual = require('./lib/devices/virtual');
 
-var switch_ = new Switch("eth2", "eth1");
+var switch1 = new Switch("eth2", "virtual");
+var switch2 = new Switch("virtual", "eth1");
+
+Virtual.connect(switch1.devices[1], switch2.devices[0]);
 
 process.on("SIGINT", function() {
   Ethernet.cleanup();
