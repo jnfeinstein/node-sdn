e4cc1b91c28239979f6365e7ba5e2a4d144ee89d
Joel <jnfeinstein@gmail.com>
2014-03-18 04:18:47 -0700

    Changed default example to a vlan bridge, where all traffic gets repeated onto the bridge.

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 require('./lib/requires');
 
-var switch1 = new Switch("eth1", "virtual-ethernet");
-var switch2 = new Switch("virtual", "virtual-ethernet");
-switch2.getDevice(0).addFilter('receive', new ChangeVlan({
-  '-': 5
-}));
+var e1 = new Ethernet('eth1');
+var e2 = new Ethernet('eth1');
 
-switch1.getDevice(1).connect(switch2.getDevice(0));
+e1.onReceived(e2.send.bind(e2));
+
+e1.addFilter('receive', function(packet) {
+  if (packet.vlan) return false;
+  return true;
+});
+e2.addFilter('send', new ChangeVlan({'-': 12}));
