1e5d016c7d522132eeac792c0632b2b25a8d0d15
Joel <jnfeinstein@gmail.com>
2014-03-17 23:40:24 -0700

    Convert preSend methods to use filters.
    I had to make the ethernet device not inherit at all for Device.prototype.send
    because I would rather not implement preSend/send/postSend if there is no
    really good need for it.  I would rather just override.
    
    I would like to add asynchronous filters in the future too.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('./lib/requires');
 
-var switch1 = new Switch("virtual", "virtual-ethernet");
+var switch1 = new Switch("eth1", "virtual-ethernet");
 var switch2 = new Switch("virtual", "virtual-ethernet");
 
 switch1.getDevice(1).connect(switch2.getDevice(0));
