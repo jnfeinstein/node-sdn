3bcc9bc7a8534353f2c4f79f8f77160cdc3f44e7
Joel <jnfeinstein@gmail.com>
2014-03-18 00:38:39 -0700

    Add changevlan filter, packet testing, some packet functions to help the changevlan filter, and a bunch of random newlines at ends of files.

--- index.js
+++ index.js
@@ -2,5 +2,8 @@ require('./lib/requires');
 
 var switch1 = new Switch("eth1", "virtual-ethernet");
 var switch2 = new Switch("virtual", "virtual-ethernet");
+switch2.getDevice(0).addFilter('receive', new ChangeVlan({
+  '-': 5
+}));
 
 switch1.getDevice(1).connect(switch2.getDevice(0));
---
+++ test/packet.js
@@ -0,0 +1,20 @@
+require('./lib/requires');
+
+exports.testChangeType = function(test) {
+  var p = new Packet();
+  p.setType(0x800);
+  test.equal(0x800, p.type);
+  test.equal(0x800, p.rawPacket.readUInt16BE(14));
+  test.done();
+};
+
+exports.testChangeVlan = function(test) {
+  var p = new Packet();
+  p.setType(0x800);
+  p.setVlan(4);
+  test.equal(p.vlan, 4);
+  test.equal(0x8100, p.rawPacket.readUInt16BE(14));
+  test.equal(p.vlan, p.rawPacket.readUInt16BE(16));
+  test.equal(0x800, p.rawPacket.readUInt16BE(18));
+  test.done();
+};
