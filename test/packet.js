03d0f4b2b5155a2ddd7833914c5aed3de05dd7ce
Joel <jnfeinstein@gmail.com>
2014-03-18 01:59:03 -0700

    Change offsets in packet because they were wrong.

--- test/packet.js
+++ test/packet.js
@@ -4,7 +4,7 @@ exports.testChangeType = function(test) {
   var p = new Packet();
   p.setType(0x800);
   test.equal(0x800, p.type);
-  test.equal(0x800, p.rawPacket.readUInt16BE(14));
+  test.equal(0x800, p.rawPacket.readUInt16BE(12));
   test.done();
 };
 
@@ -13,8 +13,8 @@ exports.testChangeVlan = function(test) {
   p.setType(0x800);
   p.setVlan(4);
   test.equal(p.vlan, 4);
-  test.equal(0x8100, p.rawPacket.readUInt16BE(14));
-  test.equal(p.vlan, p.rawPacket.readUInt16BE(16));
-  test.equal(0x800, p.rawPacket.readUInt16BE(18));
+  test.equal(0x8100, p.rawPacket.readUInt16BE(12));
+  test.equal(p.vlan, p.rawPacket.readUInt16BE(14));
+  test.equal(0x800, p.rawPacket.readUInt16BE(16));
   test.done();
 };
