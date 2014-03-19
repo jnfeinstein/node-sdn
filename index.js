f27f5b2070cffa613c28152363ea7faa407584be
Joel <jnfeinstein@gmail.com>
2014-03-19 04:49:02 -0700

    stuff

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('./lib/requires');
 
-var s = new Switch({eth0: 'eth0', eth1: 'eth2'});
-s.devices.eth0.addFilter('send', new ChangeVlan({10: null}));
-s.devices.eth1.addFilter('send', new ChangeVlan({'-': 10}));
+var s = new Switch({eth1: 'eth1', eth2: 'eth2'});
+//s.devices.eth1.addFilter('send', new ChangeVlan({'-': 12}));
+//s.devices.eth2.addFilter('send', new ChangeVlan({12: null}));
