39f612c535de2c0242f53570e0e99f43c6430543
Joel <jnfeinstein@gmail.com>
2014-04-04 00:52:56 -0700

    Update index.js since VlanBridge no longer exists.

--- index.js
+++ index.js
@@ -5,4 +5,5 @@ var s = new Switch({eth1: 'eth1', eth3: 'eth3'});
 s.devices.eth3.addFilter('send', new ChangeVlan({'-': 10}));
 s.devices.eth1.addFilter('send', new ChangeVlan({10: '-'}));
 */
-var v = new VlanBridge({'-': 10, 10: '-'}, 'eth1', 'eth3');
+var w = new Wire('eth1', 'eth3');
+w.devices[0].setNativeVlan(10);
